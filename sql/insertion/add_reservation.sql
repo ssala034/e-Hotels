daaed0a04d51ad51a88821c2f207ad5d291f34bd
YimighH34 <ming8582@hotmail.com>
2026-03-13 17:07:39 -0400

    changed add reservation to add has relation as well

--- sql/insertion/add_reservation.sql
+++ sql/insertion/add_reservation.sql
@@ -12,21 +12,29 @@ new_reservation AS (
         chain_id, hotel_id, room_num
     )
     SELECT 
-        '2026-04-01', -- Start Date
-        '2026-04-05', -- End Date
+        '2026-04-01', 
+        '2026-04-05', 
         CURRENT_TIMESTAMP, 
-        'booking',    -- Type must be 'booking' per business rules 
+        'booking', 
         'Confirmed', 
-        1,           -- The PersonID of the Customer
+        1, 
         chain_id, hotel_id, room_num
     FROM room_data
-    RETURNING reservation_id, start_date, end_date, (SELECT price FROM room_data) as daily_price
+    RETURNING reservation_id, chain_id, hotel_id, room_num, start_date, end_date, (SELECT price FROM room_data) as daily_price
+),
+booking_insert AS (
+    INSERT INTO hotel_booking (reservation_id, booked_date, future_price)
+    SELECT 
+        reservation_id, 
+        CURRENT_TIMESTAMP,
+        (end_date - start_date) * daily_price
+    FROM new_reservation
+    RETURNING reservation_id
 )
-
-INSERT INTO hotel_booking (reservation_id, booked_date, future_price)
-SELECT 
-    reservation_id, 
-    CURRENT_TIMESTAMP,
-    -- FuturePrice calculation: (EndDate - StartDate) * DailyPrice [cite: 108]
-    (end_date - start_date) * daily_price
+INSERT INTO has (chain_id, hotel_id, room_num, reservation_id)
+SELECT
+    chain_id,
+    hotel_id,
+    room_num,
+    reservation_id
 FROM new_reservation;
